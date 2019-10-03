e19b9a7fcd040eb38dee34925cd4e0b152eebef5
dan76296 <43451199+dan76296@users.noreply.github.com>
2019-10-03 14:39:57 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,30 @@
+import json
+import requests
+
+
+class HTTP:
+
+    def __init__(self, base_url, **kwargs):
+        self.base_url = base_url
+        self.session = requests.Session()
+        for arg in kwargs:
+            if isinstance(kwargs[arg], dict):
+                kwargs[arg] = self.__deep_merge(getattr(self.session, arg), kwargs[arg])
+            setattr(self.session, arg, kwargs[arg])
+        pass
+
+    def get(self, url, **kwargs):
+        return self.session.get(self.base_url+url, **kwargs)
+
+    def post(self, url, **kwargs):
+        return self.session.post(self.base_url+url, **kwargs)
+
+    @staticmethod
+    def __deep_merge(source, destination):
+        for key, value in source.items():
+            if isinstance(value, dict):
+                node = destination.setdefault(key, {})
+                HTTP.__deep_merge(value, node)
+            else:
+                destination[key] = value
+        return destination
